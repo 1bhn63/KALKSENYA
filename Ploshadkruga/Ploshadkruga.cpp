8e8d4cd4def9cc8454c43d071adc33d3518c4edc
1bhn63 <insaider101@gmail.com>
2024-02-07 13:22:26 +0300

    salava club

---
+++ Ploshadkruga/Ploshadkruga.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double calculateCircleArea(double radius) {
+    return 3.14 * radius * radius;
+}
+double calculateRingArea(double outerRadius, double innerRadius) {
+    double outerArea = calculateCircleArea(outerRadius);
+    double innerArea = calculateCircleArea(innerRadius);
+    return outerArea - innerArea;
+}
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double outerRadius, innerRadius;
+    std::cout << "Введите внешний радиус кольца: ";
+    std::cin >> outerRadius;
+    std::cout << "Введите внутренний радиус кольца: ";
+    std::cin >> innerRadius; 
+    double ringArea = calculateRingArea(outerRadius, innerRadius);
+    std::cout << "Площадь круга: " << ringArea << std::endl;
+    return 0;
+}
